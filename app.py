e9ec8ae691de307d89db5f4c4752bf2403c26f56
aesh <anton.shelashsky@techcoredev.ru>
2026-09-22 12:59:16 +0000

    Jenkinsfile + k8s deployment

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import os
-import socket
+import os, socket
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        version = os.getenv('APP_VERSION', 'unknown')
         hostname = socket.gethostname()
-        self.wfile.write(f"my-app v{version} running on {hostname}\n".encode())
+        self.wfile.write(f"my-app running on {hostname}\n".encode())
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', '8081'))
